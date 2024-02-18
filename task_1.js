a474f83b903a222453e2200f17f51f421d86c428
HerashchenkoDenys <157643762+HerashchenkoDenys@users.noreply.github.com>
2024-02-18 23:32:44 +0200

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -1,1 +1,21 @@
+let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020,
+    getInfo: function() {
+        for (let key in this) {
+            if (typeof this[key] !== 'function') { // виключаємо методи з виведення інформації
+                console.log(key + ": " + this[key]);
+            }
+        }
+    }
+};
 
+// Виведення інформації про об'єкт
+car.getInfo();
+
+// Додавання нової властивості до об'єкта
+car.color = 'blue';
+
+// Виведення інформації про об'єкт після додавання нової властивості
+car.getInfo();
